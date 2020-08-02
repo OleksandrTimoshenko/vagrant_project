5ed666fcd68c0ef3914929933df56de1b06c70a5
Oleksandr Timoshenko <56413194+Tymoshenko1999@users.noreply.github.com>
2020-08-02 14:18:05 +0300

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
 
